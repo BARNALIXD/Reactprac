5f3318d35da019a31e0ddd245a22c7b0670fa897
user.name like: "Barnali Das <user.email : dasbarnali690@gcekbpatna.ac.in>
2024-07-07 20:56:35 +0530

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import Card from './Components/Card'
 
 function App() {
+
+  const data =[
+    {name:"sam", profession : "painters", image:"https://images.unsplash.com/photo-1605811792180-61ee063690de?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"chingu", profession : "dancer", image:"https://images.unsplash.com/photo-1518834107812-67b0b7c58434?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"mingu", profession : "singer", image:"https://images.unsplash.com/photo-1460723237483-7a6dc9d0b212?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {name:"dingu", profession : "entertainer", image:"https://images.unsplash.com/photo-1470020618177-f49a96241ae7?q=80&w=988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+  ];
+
+
   return (
-    <div>
-      <Card/>
-      </div>
+    <>
+    <div className='w-full h-screen bg-zinc-300 flex gap-3 items-center justify-center'>
+    {data.map((item,index)=>(
+      <Card image={item.image}/>
+
+    ))}
+    </div>
+      </>
   )
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,24 +1,18 @@
-import React, { useState } from 'react';
-import { GrAed } from "react-icons/gr";
+import React from 'react'
 
-function Card() {
- const[val,setVal] = useState(false);
+function Card({image}) {
   return (
-    <div className='w-full h-screen bg-zinc-300 flex justify-center items-center'>
-      <div className= 'relative w-60 h-32 bg-zinc-500 rounded flex overflow-hidden'>
-       <img className={`${ val === false ? "-translate-x-[0%]": "-translate-x-[100%]"} shrink-0 w-full transistion-transform duration-300 h-full object-cover`} src="https://plus.unsplash.com/premium_photo-1709560427978-0043cfa1aee5?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img> 
-       <img className={`${ val === false ? "-translate-x-[0%]": "-translate-x-[100%]"} shrink-0 w-full transistion-transform duration-300 h-full object-cover'`}  src="https://plus.unsplash.com/premium_photo-1709560426814-d19e445c41ef?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
-      <span onClick={()=>setVal(()=>!val)} className='w-8 h-8 flex items-center justify-center bg-[#dadada8b]rounded-full  bg-zinc-200 absolute bottom-[0%] left-1/2 -translate-x-[50%] -translate-y-[50%]'>
-      <GrAed size={".8em"}/>
-      </span>
+    <div className='w-52 bg-white rounded-md overflow-hidden'>
+      <div className='w-full h-32 bg-sky-200'>
+        <img  className="w-full h-full object-cover object-[0% 20%]"src={image}/>
       </div>
-
+      <div className='w-full p-3'>
+        <h3 className='text-xl font-semibold'>Barnali</h3>
+        <h3 className='text-xs'>Profession</h3>
+        <button className='mt-4 px-3 py-1 bg-red-600 rounded text-sm text-white font-semibold'>Add friend!</button>
+      </div>
     </div>
-  )
+    )
 }
 
 export default Card
-//test
-//ghggiog
-//hgydrear
-//gvgcgvh
