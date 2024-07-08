67e115855801012264d480966bdc8277747ea540
user.name like: "Barnali Das <user.email : dasbarnali690@gcekbpatna.ac.in>
2024-07-08 11:11:38 +0530

    intialized name profession and image

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-function Card({image}) {
+function Card({image,name,profession}) {
   return (
     <div className='w-52 bg-white rounded-md overflow-hidden'>
       <div className='w-full h-32 bg-sky-200'>
         <img  className="w-full h-full object-cover object-[0% 20%]"src={image}/>
       </div>
       <div className='w-full p-3'>
-        <h3 className='text-xl font-semibold'>Barnali</h3>
-        <h3 className='text-xs'>Profession</h3>
+        <h3 className='text-xl font-semibold'>{name}</h3>
+        <h3 className='text-xs'>{profession}</h3>
         <button className='mt-4 px-3 py-1 bg-red-600 rounded text-sm text-white font-semibold'>Add friend!</button>
       </div>
     </div>
     )
 }
 
-export default Card
+export default Card 
