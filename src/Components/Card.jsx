40734540dadafd8c01cb7042b8361599595d66c5
user.name like: "Barnali Das <user.email : dasbarnali690@gcekbpatna.ac.in>
2024-07-08 11:23:54 +0530

    alert message generation onclicking the add friend button

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,7 +10,7 @@ function Card({values}) {
       <div className='w-full p-3'>
         <h3 className='text-xl font-semibold'>{name}</h3>
         <h3 className='text-xs'>{profession}</h3>
-        <button className='mt-4 px-3 py-1 bg-red-600 rounded text-sm text-white font-semibold'>Add friend!</button>
+        <button onClick={()=>alert("adding as a friend")} className='mt-4 px-3 py-1 bg-red-600 rounded text-sm text-white font-semibold'>Add friend!</button>
       </div>
     </div>
     )
