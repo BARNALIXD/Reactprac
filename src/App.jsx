d90836d26eda604a66594ac1fae1ae9f02dbe676
user.name like: "Barnali Das <user.email : dasbarnali690@gcekbpatna.ac.in>
2024-07-08 12:20:30 +0530

    dummy creation  and usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Components/Card'
 
 function App() {
 
   const data =[
-    {name:"sam", profession : "painters", image:"https://images.unsplash.com/photo-1605811792180-61ee063690de?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"chingu", profession : "dancer", image:"https://images.unsplash.com/photo-1518834107812-67b0b7c58434?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"mingu", profession : "singer", image:"https://images.unsplash.com/photo-1460723237483-7a6dc9d0b212?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
-    {name:"dingu", profession : "entertainer", image:"https://images.unsplash.com/photo-1470020618177-f49a96241ae7?q=80&w=988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+    {name:"sam", profession : "painters", image:"https://images.unsplash.com/photo-1605811792180-61ee063690de?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",friends:false},
+    {name:"chingu", profession : "dancer", image:"https://images.unsplash.com/photo-1518834107812-67b0b7c58434?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",friends:false},
+    {name:"mingu", profession : "singer", image:"https://images.unsplash.com/photo-1460723237483-7a6dc9d0b212?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",friends:false},
+    {name:"dingu", profession : "entertainer", image:"https://images.unsplash.com/photo-1470020618177-f49a96241ae7?q=80&w=988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",friends:false}
   ];
+
+ const[realdata, setrealdata]= useState(data);
+ const dummy =()=>alert("hey");
 
 
   return (
     <>
     <div className='w-full h-screen bg-zinc-300 flex gap-3 items-center justify-center'>
-    {data.map((item,index)=>(
-      <Card values={item}/>
+    {realdata.map((item,index)=>(
+      <Card key={index} dummy={dummy} values={item}/>
 
     ))}
     </div>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Card({values}) {
+function Card({values, dummy}) {
   const {name , profession, image}= values;
   return (
     <div className='w-52 bg-white rounded-md overflow-hidden'>
@@ -10,7 +10,7 @@ function Card({values}) {
       <div className='w-full p-3'>
         <h3 className='text-xl font-semibold'>{name}</h3>
         <h3 className='text-xs'>{profession}</h3>
-        <button onClick={()=>alert("adding as a friend")} className='mt-4 px-3 py-1 bg-red-600 rounded text-sm text-white font-semibold'>Add friend!</button>
+        <button onClick={dummy} className='mt-4 px-3 py-1 bg-red-600 rounded text-sm text-white font-semibold'>Add friend!</button>
       </div>
     </div>
     )
