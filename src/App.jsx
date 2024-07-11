caeef1675d5c99a1113e0a4c850f5d9223aa3ca0
user.name like: "Barnali Das <user.email : dasbarnali690@gcekbpatna.ac.in>
2024-07-11 15:25:12 +0530

    handlesubmit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React , {useState} from 'react'
 
 function App() {
   const [val,setVal] = useState({name:""});
+  const handleSubmit= (event)=>{
+    event.preventDefault();
+  }
 
   return (
     <div>
-      <form action=''>
+      <form onSubmit={handlesubmit} action=''>
         <input onChange={(event)=>setVal({name:event.target.value})} type="text" placeholder="name"/>
       </form>
     </div>
